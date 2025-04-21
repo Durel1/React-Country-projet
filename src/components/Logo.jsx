7053717a72b71bde4ab60ee50a20bab8a9c7b436
DUREL <149640202+Durel1@users.noreply.github.com>
2025-04-21 02:07:45 +0100

    Premier commit : description des modifications

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='logo'>
+        <img src="./react.svg" alt="Durel react" />
+        <h3>React world</h3>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus voluptatem nostrum alias quod neque. Libero, aspernatur, illum tempore fugit corporis cum beatae, porro pariatur assumenda adipisci iste minus amet deserunt sit. Perferendis quae ullam officiis culpa. Quis earum optio architecto vitae iure dignissimos non eos maxime labore deleniti ab vero et dolores excepturi reiciendis molestiae nulla, ratione soluta praesentium vel voluptatibus numquam repudiandae? Nam doloremque iure qui ea maiores nesciunt natus ratione. Fugit nobis minima qui eius quo, pariatur repudiandae temporibus quas magnam esse repellat consequatur sint voluptatibus voluptatem. Fugiat ipsam nam dolorum incidunt quo debitis dolores maiores aspernatur neque?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, sit? Sit nam eius assumenda sed, voluptatum accusantium cum ipsum commodi dicta modi dolor fugit delectus recusandae unde, nulla harum veritatis.</p>
+        </div>
+    );
+};
+
+export default About;
